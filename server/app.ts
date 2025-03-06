5bc4f2bc56ef4777ed6c4d9c9d37e88af2b9a0cb
bekeeeee <m.ragab9521@gmail.com>
2025-03-06 09:30:02 +0200

    add send echo method

--- server/app.ts
+++ server/app.ts
@@ -25,7 +25,6 @@ HTTP_SERVER.listen(CONSTANTS.PORT, () => {
 CONSTANTS.CUSTOM_ERRORS.forEach(error => {
   process.on(error, err => {
     console.error(`Error: ${error} => ${err}`);
-
     // exit the server
     process.exit(1);
   });
@@ -106,7 +105,8 @@ class WebSocketReceiver {
   }
   processBuffer(chunk: Buffer) {
     this._buffersArray.push(chunk);
-    this._bufferedBytesLength += chunk.length;
+    this._bufferedBytesLength += chunk?.length;
+    console.log("Chunk received of size: " + chunk.length);
 
     this.startTaskLoop();
   }
@@ -133,68 +133,48 @@ class WebSocketReceiver {
     } while (this._taskLoop);
   }
 
-  private _sendEcho() {}
-  private _getPayload() {
-    // *** LOOP for the full frame payload
-    // if we have not yet received the payload, wait for another 'data' event fired on our socket object, in order to receive more payload
-    if (this._bufferedBytesLength < this._framePayloadLength) {
+  private _getInfo() {
+    // check whether we have enough bytes in our internal buffer to process at the very least frame header information
+    if (this._bufferedBytesLength < CONSTANTS.MIN_FRAME_SIZE) {
+      // wait for additional chunks via the 'data' event on our socket object
       this._taskLoop = false;
       return;
     }
-    // FULL FRAME RECEIVED (there may be more frames if we have a fragmented message)
-    this._framesReceived++;
-
-    // extract the actual payload data
-    const payloadBuffer = this._consumePayload(this._framePayloadLength);
-    // unmask the payload data
-    const unmaskedPayload = FUNCTIONS.unmaskPayload(payloadBuffer, this._mask);
-    console.log("unmasked payload: " + unmaskedPayload.toString("utf8"));
-    // TEXT FRAME
-    // push decode / unmasked data into our fragments array
-    if (payloadBuffer.length) {
-      this._fragments.push(unmaskedPayload);
+    const infoBuffer = this._consumeHeaders(CONSTANTS.MIN_FRAME_SIZE);
+    const firstByte = infoBuffer[0];
+    const secondByte = infoBuffer[1];
+    // extract WS payload information
+    this._fin = (firstByte & 0x80) === 0x80;
+    this._opcode = firstByte & 0x0f;
+    this._masked = (secondByte & 0x80) === 0x80;
+    this._initialPayloadSizeIndicator = secondByte & 0x7f;
+    // if data is not masked, then throw an error
+    if (!this._masked) {
+      throw Error("Received data is not masked");
     }
-    // if this is the final fragment, then we can join all the fragments together
-    if (this._fin) {
-      console.log(
-        "Total frames received in this WS message: " + this._framesReceived
-      );
-      console.log(
-        "Total Payload size of the WS message is: " + this._totalPayloadLength
-      );
-      this._task = SEND_ECHO;
-      // const message = Buffer.concat(this._fragments).toString();
-      // console.log("Message: " + message);
-      // this._fragments = [];
-    } else {
-      this._task = GET_INFO;
-    }
-    // send the unmasked payload data to the client
-    // this._socket.write(unmaskedPayload);
-    // this._task = SEND_ECHO;
+    this._task = GET_LENGTH;
   }
-  private _consumePayload(bytesLength: number) {
+  private _consumeHeaders(bytesLength: number): Buffer<ArrayBufferLike> {
+    // reduce our buffered bytes length by how many bytes we will consume
     this._bufferedBytesLength -= bytesLength;
 
-    const payloadBuffer = Buffer.alloc(bytesLength); // creating a new buffer for data we are yet to put into it.
-    let totalBytesRead = 0;
-    while (totalBytesRead < bytesLength) {
-      const buf = this._buffersArray[0]; // retrieve the first chunk of data from an array of chunks
-      const bytesToRead = Math.min(bytesLength - totalBytesRead, buf.length); // calculate the number of bytes to read from buf, ensuring that it does not access the reaming bytes needed to reach bytesLength
-      buf.copy(payloadBuffer, totalBytesRead, 0, bytesToRead); // copy the bytes from buf to payloadBuffer
-      totalBytesRead += bytesToRead; // increment the totalBytesRead by the number of bytes read from buf
-      if (bytesToRead < buf.length) {
-        this._buffersArray[0] = buf.slice(bytesToRead);
-      } else {
-        this._buffersArray.shift(); // remove the entire first element in the array
-      }
+    if (bytesLength === this._buffersArray[0]?.length) {
+      return this._buffersArray.shift() as Buffer<ArrayBufferLike>;
     }
-    return payloadBuffer;
+
+    if (bytesLength < this._buffersArray[0]?.length) {
+      // create a temporary info buffer from the _buffersArray
+      let infoBuffer = this._buffersArray[0];
+      // remove consumed bytes from our buffer
+      this._buffersArray[0] = this._buffersArray[0].slice(bytesLength);
+      return infoBuffer.slice(0, bytesLength);
+    } else {
+      throw Error(
+        "You cannot extract more data from a ws frame than the actual frame size."
+      );
+    }
   }
-  private _getMaskKey() {
-    this._mask = this._consumeHeaders(CONSTANTS.MASK_LENGTH);
-    this._task = GET_PAYLOAD;
-  }
+
   private _getLength() {
     // extract the actual payload length of the WS message
     switch (this._initialPayloadSizeIndicator) {
@@ -237,38 +217,142 @@ class WebSocketReceiver {
       throw Error("Received data is not masked");
     }
   }
-
-  private _getInfo() {
-    const infoBuffer = this._consumeHeaders(CONSTANTS.MIN_FRAME_SIZE);
-    const firstByte = infoBuffer[0];
-    const secondByte = infoBuffer[1];
-    // extract WS payload information
-    this._fin = (firstByte & 0x80) === 0x80;
-    this._opcode = firstByte & 0x0f;
-    this._masked = (secondByte & 0x80) === 0x80;
-    this._initialPayloadSizeIndicator = secondByte & 0x7f;
-    // if data is not masked, then throw an error
-    if (!this._masked) {
-      throw Error("Received data is not masked");
-    }
-    this._task = GET_LENGTH;
+  private _getMaskKey() {
+    this._mask = this._consumeHeaders(CONSTANTS.MASK_LENGTH);
+    this._task = GET_PAYLOAD;
   }
-  private _consumeHeaders(bytesLength: number): Buffer<ArrayBufferLike> {
-    // reduce our buffered bytes length by how many bytes we will consume
+  private _getPayload() {
+    // *** LOOP for the full frame payload
+    // if we have not yet received the payload, wait for another 'data' event fired on our socket object, in order to receive more payload
+    if (this._bufferedBytesLength < this._framePayloadLength) {
+      this._taskLoop = false;
+      return;
+    }
+    // FULL FRAME RECEIVED (there may be more frames if we have a fragmented message)
+    this._framesReceived++;
+
+    // extract the actual payload data
+    const payloadBuffer = this._consumePayload(this._framePayloadLength);
+    // unmask the payload data
+    const unmaskedPayload = FUNCTIONS.unmaskPayload(payloadBuffer, this._mask);
+    // console.log("unmasked payload: " + unmaskedPayload.toString("utf8"));
+    // TEXT FRAME
+    // push decode / unmasked data into our fragments array
+    if (payloadBuffer?.length) {
+      this._fragments.push(unmaskedPayload);
+    }
+    // if this is the final fragment, then we can join all the fragments together
+    if (this._fin) {
+      console.log(
+        "Total frames received in this WS message: " + this._framesReceived
+      );
+      console.log(
+        "Total Payload size of the WS message is: " + this._totalPayloadLength
+      );
+      this._task = SEND_ECHO;
+      // const message = Buffer.concat(this._fragments).toString();
+      // console.log("Message: " + message);
+      // this._fragments = [];
+    } else {
+      this._task = GET_INFO;
+    }
+  }
+
+  private _consumePayload(bytesLength: number) {
     this._bufferedBytesLength -= bytesLength;
 
-    if (bytesLength === this._buffersArray[0].length) {
-      return this._buffersArray.shift() as Buffer<ArrayBufferLike>;
+    const payloadBuffer = Buffer.alloc(bytesLength); // creating a new buffer for data we are yet to put into it.
+    let totalBytesRead = 0;
+
+    while (totalBytesRead < bytesLength) {
+      const buf = this._buffersArray[0]; // retrieve the first chunk of data from an array of chunks
+      const bytesToRead = Math.min(bytesLength - totalBytesRead, buf.length); // calculate the number of bytes to read from buf, ensuring that it does not access the reaming bytes needed to reach bytesLength
+
+      buf.copy(payloadBuffer, totalBytesRead, 0, bytesToRead); // copy the bytes from buf to payloadBuffer
+      totalBytesRead += bytesToRead; // increment the totalBytesRead by the number of bytes read from buf
+
+      if (bytesToRead < buf.length) {
+        this._buffersArray[0] = buf.slice(bytesToRead);
+      } else {
+        this._buffersArray.shift(); // remove the entire first element in the array
+      }
     }
-    if (bytesLength < this._buffersArray[0].length) {
-      let buffer = this._buffersArray[0];
-      let result = buffer.slice(0, bytesLength);
-      this._buffersArray[0] = buffer.slice(bytesLength);
-      return result;
+    return payloadBuffer;
+  }
+  private _sendEcho() {
+    // **** TASK 1: CONSTRUCT AN EMPTY FRAME WITH CORRECT SIZE ****
+    // extract our entire message (could consist of numerous frame) from our persistent _fragments array, and create ONE buffer with the entire message.
+    const fullMessage = Buffer.concat(this._fragments); // this is the actual payload of our WS frame
+    let payloadLength = fullMessage.length;
+    let additionalPayloadSizeIndicator = null;
+    // determine the additional bytes required to represent the payload size
+    switch (true) {
+      case payloadLength <= CONSTANTS.SMALL_DATA_SIZE:
+        additionalPayloadSizeIndicator = 0;
+        break;
+      case payloadLength > CONSTANTS.SMALL_DATA_SIZE &&
+        payloadLength <= CONSTANTS.MEDIUM_DATA_SIZE:
+        additionalPayloadSizeIndicator = CONSTANTS.MEDIUM_SIZE_CONSUMPTION;
+        break;
+      default:
+        additionalPayloadSizeIndicator = CONSTANTS.LARGE_SIZE_CONSUMPTION;
+        break;
+    }
+    const frame = Buffer.alloc(
+      CONSTANTS.MIN_FRAME_SIZE + additionalPayloadSizeIndicator + payloadLength
+    );
+    // populate the frame with all header info
+    // first byte
+    let fin = 0x01;
+    let rev1 = 0x00;
+    let rev2 = 0x00;
+    let rev3 = 0x00;
+    let opcode = CONSTANTS.OPCODE_BINARY;
+    let firstByte =
+      (fin << 7) | (rev1 << 6) | (rev2 << 5) | (rev3 << 4) | opcode;
+    frame[0] = firstByte;
+    // second byte
+    let mask = 0x00;
+    // let secondByte = 0x00;
+    if (payloadLength <= CONSTANTS.SMALL_DATA_SIZE) {
+      frame[1] = mask | payloadLength;
+    } else if (
+      payloadLength > CONSTANTS.SMALL_DATA_SIZE &&
+      payloadLength <= CONSTANTS.MEDIUM_DATA_SIZE
+    ) {
+      frame[1] = mask | CONSTANTS.MEDIUM_DATA_FLAG;
+      frame.writeUInt16BE(payloadLength, CONSTANTS.MIN_FRAME_SIZE);
     } else {
-      throw Error(
-        "You cannot extract more data from a ws frame than the actual frame size."
-      );
+      frame[1] = mask | CONSTANTS.LARGE_DATA_FLAG;
+      frame.writeBigUInt64BE(BigInt(payloadLength), CONSTANTS.MIN_FRAME_SIZE);
     }
+
+    // add payload to the frame
+    const messageStartOffset =
+      CONSTANTS.MIN_FRAME_SIZE + additionalPayloadSizeIndicator;
+    fullMessage.copy(frame, messageStartOffset);
+    // send the frame to the client and reset the values
+    this._socket.write(frame);
+    this._reset();
+  }
+
+  private _reset() {
+    this._buffersArray = [];
+    this._bufferedBytesLength = 0;
+
+    this._taskLoop = false;
+    this._task = GET_INFO;
+    this._fin = false;
+
+    this._opcode = 0;
+    this._masked = false;
+    this._initialPayloadSizeIndicator = 0;
+
+    this._framePayloadLength = 0;
+    this._totalPayloadLength = 0; // indicates the total length of the payload
+    this._mask = Buffer.alloc(CONSTANTS.MASK_LENGTH); // this will hold the masking key set and sent by the client
+
+    this._framesReceived = 0; // tally of how many frames were received
+    this._fragments = [];
   }
 }
--- server/src/custom_library/websocket_constants.ts
+++ server/src/custom_library/websocket_constants.ts
@@ -10,8 +10,17 @@ export const CONSTANTS = {
   MIN_FRAME_SIZE: 2,
   MASK_LENGTH: 4,
 
+  SMALL_DATA_SIZE: 125,
+  MEDIUM_DATA_SIZE: 65535,
   MEDIUM_DATA_FLAG: 126,
   LARGE_DATA_FLAG: 127,
   MEDIUM_SIZE_CONSUMPTION: 2,
   LARGE_SIZE_CONSUMPTION: 8,
+
+  // *** WEBSOCKET OPCODES
+  OPCODE_TEXT: 0x01, // text frame
+  OPCODE_BINARY: 0x02, // binary frame
+  OPCODE_CLOSE: 0x08, // closure frame
+  OPCODE_PING: 0x09, // ping frame
+  OPCODE_PONG: 0x0a, // pong frame
 };
